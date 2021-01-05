8a9b05d820048e2845f4fe6deeb70d2a9684f910
etk66296 <etk66296@web.de>
2021-01-05 19:21:40 +0100

    progress in current velocity

--- bla/src/pages/physics/newtonianMechanics/CurrentVelocity.vue
+++ bla/src/pages/physics/newtonianMechanics/CurrentVelocity.vue
@@ -13,6 +13,7 @@
 
 <script>
 import {VueMathjax} from 'vue-mathjax'
+// import * as dat from 'dat.gui'
 import "phaser"
 export default {
   name: 'NaturalNumbers',
@@ -66,8 +67,44 @@ export default {
     }
 
     let heli = null
-    let heliFollower = null
-    let graphics = null
+    let heliCurrentSpeed = 0
+    let marginLeft = 25
+
+
+    // cartesian -->
+    let cartesian = {
+      origin: null,
+      xAxisSegments: null,
+      yAxisSegments: null,
+      xArrow: null,
+      yArrow: null,
+      labelX: 't',
+      labelY: 'x'
+    }
+
+    let drawCartesian = (scene, x, y, lengthX, lengthY) => {
+      cartesian.origin = scene.add.image(x, y, 'origin')
+      cartesian.xArrow = scene.add.image(x + lengthX * 32, y, 'arrowXHead')
+      cartesian.labelX = scene.add.text(x + lengthX * 32, y + 8, 't' , { font: '16px', fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'})
+      cartesian.yArrow = scene.add.image(x, y + lengthY * (-32), 'arrowYHead')
+      cartesian.labelY = scene.add.text(x + 8, y + lengthY * (-32) - 16, 'x' , { font: '16px', fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'})
+      cartesian.xAxisSegments = scene.add.group({
+        key: 'arrowXSegment',
+        repeat: lengthX - 2,
+        setXY: { x: x + 32, y: y, stepX: 32 }
+      })
+      cartesian.yAxisSegments = scene.add.group({
+        key: 'arrowYSegment',
+        repeat: lengthY - 2,
+        setXY: { x: x, y: y - 32, stepY: -32 }
+      })
+    }
+    // <-- cartesian
+
+    let infoText = null
+    let infoText2 = null
+    let nextButton = null
+    let step = 0
 
     function create () {
       this.cameras.main.setBackgroundColor(0x121212)
@@ -85,81 +122,147 @@ export default {
       heli = this.physics.add.sprite(-50, config.height * 0.5, 'helicopter')
       heli.body.allowGravity = false
       heli.play('fly')
-      heli.setVelocityX(80)
+      heli.setVelocityX(0)
 
-      graphics = this.add.graphics({ lineStyle: { width: 1, color: 0x00ff00 }, fillStyle: { color: 0x0000aa } });
-    
+      drawCartesian(this, Math.floor(config.width * 0.0) + marginLeft, Math.floor(config.height * 0.4), Math.floor(config.width * 0.8 / 32) - 2, Math.floor(config.height * 0.4 / 32) - 2)
+      
+      // info text
+      infoText = this.add.text(config.width * 0.51, config.height * 0.2, [''] , { font: isPortrait ? String(Math.floor(config.width * 0.04)) + 'px' : String(Math.floor(config.height * 0.035)) + 'px' , fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'})
+      infoText.setDepth(10000)
+      infoText.setText(['x sei die Position', 'des Helikopter'])
+
+      infoText2 = this.add.text(config.width * 0.1, config.height * 0.6, [''] , { font: isPortrait ? String(Math.floor(config.width * 0.04)) + 'px' : String(Math.floor(config.height * 0.035)) + 'px' , fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'})
+      infoText2.setDepth(10000)
+      
+
+      // next button
+      nextButton = this.add.sprite(Math.floor(config.width * 0.5), config.height, 'nextButton', 'idlebutton').setInteractive({ useHandCursor: true })
+      nextButton.scale = isPortrait ? config.height * 0.001 : config.width * 0.001
+      nextButton.y -= Math.floor((nextButton.height - 5) * nextButton.scale)
+      nextButton.on('pointerover', (pointer) => {
+        nextButton.setFrame('hoveredbutton')
+      })
+      nextButton.on('pointerout', (pointer) => {
+        nextButton.setFrame('idlebutton')
+      })
+        nextButton.on('pointerdown', (pointer) => {
+        nextButton.setFrame('pressedbutton')
+      })
+      nextButton.on('pointerup', (pointer) => {
+        step++
+      })
+      nextButton.setActive(false)
+      nextButton.setVisible(false)
     }
     
-    let measurePoints = []
-    let measurePointConnectors = []
-    let step = 0
-    let measurePointPos = { x: 0, y: 0 }
-    let originY = 0
-    let deltaV = 0
-    let tmpV = 0
-    let blockUpdate = false
+    let measurepoints = []
+    
+    let virtualTime = 0
+    let trackHeli = (scene) => {
+      // mark the heli translation
+      virtualTime += 0.2
+      measurepoints.push(scene.add.rectangle(virtualTime + marginLeft, Math.floor(config.width * 0.2 - heli.x * 0.125), 2, 2, 0x00ff00))      
+    }
 
+    let gui = null
+    let camera1 = null
 
     function update (time, delta) {
       switch(step) {
         case 0:
-          measurePoints.push(this.add.rectangle(measurePointPos.x,  heli.y + 20, 4, 4, 0x00ff00))
-          measurePointPos.x++
-          originY = heli.y + 20
-          if (heli.x >= config.width * 0.2) {
+          heliCurrentSpeed++
+          heli.setVelocityX(heliCurrentSpeed)
+          heli.setRotation(0.15)
+          infoText.setText(['der Helikopter', 'beschleunigt in', 'Vorwärts-Richtung'])
+          trackHeli(this)
+          if (heli.x >= Math.floor(config.width * 0.2)) {
             step++
           }
         break
         case 1:
-          if (!blockUpdate) {
-            let curve = new Phaser.Curves.Ellipse(0, 0, Math.floor(config.width * 0.3), Math.floor(config.height * 0.3), 90, 91, true, true)
-            heliFollower = this.add.follower(curve, heli.x, heli.y, 'helicopter');
-            heliFollower.play('fly')
-            measurePointPos.x++
-            blockUpdate = true
-            this.children.remove(heli)
-            heliFollower.startFollow({
-              duration: 8000,
-              yoyo: false,
-              repeat: 1,
-              rotateToPath: true,
-              verticalAdjust: true,
-              onComplete: () => {
-                let heliPos = { x: heliFollower.x, y: heliFollower.y }
-                this.children.remove(heliFollower)
-                heli = this.physics.add.sprite(heliPos.x, heliPos.y, 'helicopter')
-                heli.body.allowGravity = false
-                heli.play('fly')
-                heli.setVelocityX(80)
-                step++
-                blockUpdate = false
-              }
-            })
-          } else {
-            deltaV = heliFollower.y - tmpV
-            measurePointPos.x = measurePointPos.x + 0.2
-            measurePointPos.y = originY - Math.floor(deltaV * 20)
-            measurePoints.push(this.add.rectangle(Math.floor(measurePointPos.x),  measurePointPos.y, 4, 4, 0x00ff00))
-            tmpV = heliFollower.y
+          heliCurrentSpeed--
+          heli.setRotation(-0.15)
+          heli.setVelocityX(heliCurrentSpeed)
+          infoText.setText(['er bremst ab', 'und beschleunigt', 'Rückwärts'])
+          trackHeli(this)
+           if (heli.x <= (-1) * Math.floor(config.width * 0.05)) {
+            step++
+            heli.setRotation(0.1)
           }
         break
         case 2:
-          measurePoints.push(this.add.rectangle(measurePointPos.x,  heli.y + 20, 4, 4, 0x00ff00))
-          measurePointPos.x++
-          if (heli.x >= (config.width + 50)) {
+          heliCurrentSpeed++
+          heli.setVelocityX(heliCurrentSpeed)
+          heli.setRotation(0.15)
+          infoText.setText(['und ...', 'wieder auf den', 'Bildschirm'])
+          trackHeli(this)
+          if (heli.x >= Math.floor(config.width * 0.1)) {
             step++
+            infoText.setText(['und ...', 'wieder auf den', 'Bildschirm'])
           }
         break
         case 3:
-          step++
+          heliCurrentSpeed -= 5
+          heli.setRotation(-0.15)
+          heli.setVelocityX(heliCurrentSpeed)
+          infoText.setText(['langsaaaam!😄'])
+          trackHeli(this)
+           if (heli.body.velocity.x <= 25) {
+            infoText.setText(['weiter mit', 'konstanter', 'Geschwindigkeit'])
+            step++
+            heli.setRotation(0.0)
+            heli.setRotation(0)
+            heli.setVelocityX(25)
+          }
           break
         case 4:
+          trackHeli(this)
+           if (heli.x >= Math.floor(config.width * 0.5)) {
+            infoText.setText(['uuund Stopp', 'Position halten'])
+            step++
+            heli.setRotation(0.0)
+            heli.setRotation(0)
+            heli.setVelocityX(0)
+            nextButton.setActive(true)
+            nextButton.setVisible(true)
+          }
+        break
+        case 5:
+          infoText.setText(['die Kurve zeigt', 'die Verschiebung x', 'des Helikopters', 'nach der Zeit'])
+          infoText2.setText([
+            'Geschwindigkeit ist definiert als',
+            '     Δx',
+            'v = ―――',
+            '     Δt',
+            'entnimmt man über die Gesamte Kurve alle infinitesimal',
+            'kleinen Zeitabschnitt '
+          ])
+          // // gui = new dat.GUI()
+          // // camera1 = this.cameras.main;
+          // camera1 = this.cameras.add(0, 0, Math.floor(config.width * 0.5), Math.floor(config.height * 0.5)).setZoom(0.5);
+          // gui.addFolder('Camera 1')
+          // gui.add(camera1, 'x')
+          // gui.add(camera1, 'y')
+          // gui.add(camera1, 'width')
+          // gui.add(camera1, 'height')
+          // gui.add(camera1, 'centerToSize')
+          // gui.add(camera1, 'scrollX', -1920, 1920)
+          // gui.add(camera1, 'scrollY', -989, 989)
+          // gui.add(camera1, 'zoom', 0.1, 2).step(0.1)
+          // gui.add(camera1, 'rotation').step(0.01)
+          // gui.addColor(camera1, 'backgroundColor').onChange(function (value) {
+          //   value.a = 255
+          //   camera1.setBackgroundColor(value)
+          // })
+          // step++
+
+        break
+        case 6:
         break
         default:
         break
       }
-
+      heli.setVelocityY(Math.sin(time) * 15)
     }
   }
 }
